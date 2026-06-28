863cdc9406cdcf7649f3bf1ece9d4c36b6387d53
crtn333m-dotcom <crtn333m@gmail.com>
2026-06-28 15:27:01 +0300

    Refactor build.gradle.kts to use plugins
    Removed allprojects and subprojects configurations, and added plugin declarations for Android application, Kotlin, and Google services.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,5 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: File = rootProject.layout.buildDirectory.dir("../../build").get().asFile
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: File = newBuildDir.resolve(project.name)
-    project.layout.buildDirectory.set(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
