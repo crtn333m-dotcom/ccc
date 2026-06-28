89945cd9d7b3fa8551698fd0d655611ae9406945
crtn333m-dotcom <crtn333m@gmail.com>
2026-06-28 06:18:46 +0300

    Remove android configuration and add dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,4 @@
-android {
-    namespace = "com.example.haway_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    defaultConfig {
-        applicationId = "com.example.haway_app"
-        minSdkVersion 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+dependencies {
+    classpath 'com.android.tools.build:gradle:8.1.0'
+    classpath 'com.google.gms:google-services:4.4.1'
 }
