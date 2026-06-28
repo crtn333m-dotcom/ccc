3be9e298fea52c20661a30bc8b3f79fddfec782b
crtn333m-dotcom <crtn333m@gmail.com>
2026-06-28 12:40:34 +0300

    Update build.gradle.kts for new build directory
    Refactor build directory structure and add repositories.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,21 @@
-dependencies {
-    classpath 'com.android.tools.build:gradle:8.1.0'
-    classpath 'com.google.gms:google-services:4.4.1'
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: File = rootProject.layout.buildDirectory.dir("../../build").get().asFile
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: File = newBuildDir.resolve(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
